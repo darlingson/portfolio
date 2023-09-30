df09cfe85c16d4f0c4bd8ca5990f0ddd5f275cc2
darlingson <dmakuwila@gmail.com>
2023-09-30 19:52:32 +0200

    added navbar

---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+const Intro = (params) => {
+    return(<>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-4">
+                    <img src="/12511668-kg.jpg" />
+                </div>
+                <div className="col-sm-8">
+                    <div class="d-flex align-content-center flex-wrap">
+                        <p>Hello, My name is Darlingson Makuwila. System Developer and FullStack Web Developer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>);
+}
+export default Intro;
