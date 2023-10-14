4e2b66fff40bb9797d003ecbcaf9865000e25b97
darlingson <dmakuwila@gmail.com>
2023-10-14 15:44:24 +0200

    added styling to tools and projects lists

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,34 @@
 function Projects(){
     return(<>
         <h1>Projects</h1>
-        <div className="container">
-            <p>
-                <h3>My Projects</h3>
-                <ul>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                    <li>Project 3</li>
-                </ul>
-            </p>
+        <div className="container grid-container">
+            <div className="grid-item">
+                <ProjectTitle title="Text to Chichewa Sign Language Translator"/>
+                <ProjectDesc desc="This is a Translator which converts text to chichewa sign language"/>
+            </div>
+            <div className="grid-item">
+                <ProjectTitle title="Takulandilani App"/>
+                <ProjectDesc desc="App built with purpose of the helping users find information about Malawian Tourism. The app has a list of places to visit, accomodation and transportation"/>
+            </div>
+            <div className="grid-item">
+                <ProjectTitle title="Chikulunga Analytics"/>
+                <ProjectDesc desc="App built as part of the data science learning journey. Its mainly showing how malawian football is going in the Super league of Malawi."/>
+            </div>
+            <div className="grid-item">
+                <ProjectTitle title="MUBAS Past Papers Repository"/>
+                <ProjectDesc desc="A project I worked on for my final year project. The project is a web portal, where students and teachers can access past papers"/>
+            </div>
         </div>
     </>);
 }
 export default Projects;
+function ProjectTitle(props){
+    return(<>
+        <h3>{props.title}</h3>
+    </>);
+}
+function ProjectDesc(props){
+    return(<>
+        <p>{props.desc}</p>
+    </>);
+}
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,36 +1,36 @@
 function Tools() {
     return (<>
         <h1>Tools</h1>
-        <div className="container">
-            <p>
+        <div className="container grid-container">
+            <div className="grid-item">
                 <h3>I know the following Languages</h3>
-                <ul>
-                    <li>Java</li>
-                    <li>Python</li>
-                    <li>JavaScript</li>
-                    <li>GO</li>
-                    <li>Kotling</li>
-                    <li>Dart</li>
+                <ul className ="list-group">
+                    <li className ="list-group-item">Java</li>
+                    <li className ="list-group-item">Python</li>
+                    <li className ="list-group-item">JavaScript</li>
+                    <li className ="list-group-item">GO</li>
+                    <li className ="list-group-item">Kotling</li>
+                    <li className ="list-group-item">Dart</li>
                 </ul>
-            </p>
-            <p>
+            </div>
+            <div className="grid-item">
                 <h3>I know the following Frameworks and Libraries</h3>
-                <ul>
-                    <li>React</li>
-                    <li>Laravel</li>
-                    <li>Django</li>
-                    <li>Flask</li>
-                    <li>Flutter</li>
+                <ul className ="list-group">
+                    <li className ="list-group-item">React</li>
+                    <li className ="list-group-item">Laravel</li>
+                    <li className ="list-group-item">Django</li>
+                    <li className ="list-group-item">Flask</li>
+                    <li className ="list-group-item">Flutter</li>
                 </ul>
-            </p>
-            <p>
+            </div>
+            <div className="grid-item">
                 <h3>I know the following Tools</h3>
-                <ul>
-                    <li>Git/Github</li>
-                    <li>MySQL/MariaDB</li>
-                    <li>Firebase</li>
+                <ul className ="list-group">
+                    <li className ="list-group-item">Git/Github</li>
+                    <li className ="list-group-item">MySQL/MariaDB</li>
+                    <li className ="list-group-item">Firebase</li>
                 </ul>
-            </p>
+            </div>
         </div>
     </>);
 }
