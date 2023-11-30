6e591683d2809480133880a71955ad1abb30bcb5
darlingson <dmakuwila@gmail.com>
2023-11-30 23:03:20 +0200

    styled the links to social media

--- src/components/SocialMediaContact.jsx
+++ src/components/SocialMediaContact.jsx
@@ -2,22 +2,22 @@ const SocialMediaContact =()=>{
     return(<>
         <div class="mx-auto w-1/2 text-white text-center p-4 flex">
             <div>
-                <a>
+                <a href="https://www.linkedin.com/in/darlingsonm/">
                     <img src='/linkedin_logo_icon_259377.png' className="w-100 h-70"/>
                 </a>
             </div>
             <div>
-                <a>
+                <a href="https://twitter.com/dmakuwila">
                     <img src="/x_twitter_logo_icon_259367.png" className="w-100 h-70" />
                 </a>
             </div>
             <div>
-                <a>
+                <a href="github.com/darlingson">
                     <img src="/github-logo_icon-icons.com_73546.png" className="w-100 h-70"/>
                 </a>
             </div>
             <div>
-                <a>
+                <a href="https://www.kaggle.com/datasets/makuwilad">
                     <img src="/kaggle_logo_icon_168474.png" className="w-100 h-70"/>
                 </a>
             </div>
