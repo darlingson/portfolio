335fd1b389a5bd7fe6a570b8532160764e74210f
darlingson <dmakuwila@gmail.com>
2023-12-14 21:19:55 +0200

    made the intro mobile responsive

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,11 +3,11 @@ import Typewriter from 'typewriter-effect';
 
 const Intro = () => {
     return (
-        <div id="intro" className="container mx-auto flex">
-            <div className="w-1/2">
+        <div id="intro" className="container mx-auto flex flex-col md:flex-row">
+            <div className="w-full md:w-1/2">
                 <img className="h-auto max-w-full" src="/12511668-kg.jpg" alt="Darlingson Makuwila"/>
             </div>
-            <div className="w-1/2 flex items-center">
+            <div className="w-full md:w-1/2 flex items-center">
                 <Typewriter
                     options={{
                         strings: ["Hello there! I’m Darlingson Makuwila. In the tech world, you’ll find me wearing three hats - one of a System Developer, another of a FullStack Web Developer, and the third of an App Developer. I love diving into systems, untangling complexities, and creating solutions that make a difference."],
