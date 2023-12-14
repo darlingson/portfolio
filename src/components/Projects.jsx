52a577f32d80b13c4471f98708153c2138a31166
darlingson <dmakuwila@gmail.com>
2023-12-14 21:25:43 +0200

    made the projects and tooks components responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,42 @@
-function Projects(){
-    return(<>
-    <div className="mx-auto">
-      <h1 class="">Projects</h1>
-      <hr class="border-t-2 border-blue-500 my-2" />
-      <div id="projects" class="container grid grid-cols-3 gap-2">
-        <div class=" bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
-          <h3 class="font-bold">Text to Chichewa Sign Language Translator</h3>
-          <p><a href="http://text-malawisign.000webhostapp.com/view/home.html" class="text-blue-500 hover:underline">Project Link</a></p>
-          <p>This is a Translator which converts text to chichewa sign language</p>
+import React from 'react';
+
+function Projects() {
+    return (
+        <div className="mx-auto">
+            <h1>Projects</h1>
+            <hr className="border-t-2 border-blue-500 my-2" />
+            <div id="projects" className="container grid grid-cols-1 md:grid-cols-3 gap-2">
+                <Project title="Text to Chichewa Sign Language Translator" desc="This is a Translator which converts text to chichewa sign language" link="http://text-malawisign.000webhostapp.com/view/home.html" />
+                <Project title="Takulandilani App" desc="App built with purpose of the helping users find information about Malawian Tourism. The app has a list of places to visit, accomodation and transportation" />
+                <Project title="Chikulunga Analytics" desc="App built as part of the data science learning journey. Its mainly showing how malawian football is going in the Super league of Malawi." />
+                <Project title="MUBAS Past Papers Repository" desc="A project I worked on for my final year project. The project is a web portal, where students and teachers can access past papers" />
+            </div>
         </div>
-        <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 p-2">
-          <h3 class="font-bold">Takulandilani App</h3>
-          <p>App built with purpose of the helping users find information about Malawian Tourism. The app has a list of places to visit, accomodation and transportation</p>
-        </div>
-        <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 p-2">
-          <h3 class="font-bold">Chikulunga Analytics</h3>
-          <p>App built as part of the data science learning journey. Its mainly showing how malawian football is going in the Super league of Malawi.</p>
-        </div>
-        <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 p-2">
-          <h3 class="font-bold">MUBAS Past Papers Repository</h3>
-          <p>A project I worked on for my final year project. The project is a web portal, where students and teachers can access past papers</p>
-        </div>
-      </div>
-      </div>
-    </>);
+    );
 }
+
+function Project(props) {
+    return (
+        <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+            <ProjectTitle title={props.title} />
+            <ProjectDesc desc={props.desc} link={props.link} />
+        </div>
+    );
+}
+
+function ProjectTitle(props) {
+    return (
+        <h3 className="font-bold">{props.title}</h3>
+    );
+}
+
+function ProjectDesc(props) {
+    return (
+        <>
+            <p>{props.desc}</p>
+            <p className="rose-500"><a href={props.link} className="text-rose-500 hover:bg-rose-700">Check it out</a></p>
+        </>
+    );
+}
+
 export default Projects;
-function ProjectTitle(props){
-    return(<>
-        <h3>{props.title}</h3>
-    </>);
-}
-function ProjectDesc(props){
-    return(<>
-        <p>{props.desc}</p>
-        <p className="rose-500"><a href={props.link} className="text-rose-500 hover:bg-rose-700">Check it out</a></p>
-    </>);
-}
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,40 +1,43 @@
-function Tools() {
-    return (<>
-    <div className="container mx-auto">
-      <h1>Tools</h1>
-      <hr class="border-t-2 border-blue-500 my-2" />
-      <div id="tools" class=" my-2 container grid grid-cols-3 gap-2 justify-center">
-        <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
-          <h3>I know the following Languages</h3>
-            <ul class="list-none">
-              <li class="border border-gray-200 p-2">Java</li>
-              <li class="border border-gray-200 p-2">Python</li>
-              <li class="border border-gray-200 p-2">JavaScript</li>
-              <li class="border border-gray-200 p-2">GO</li>
-              <li class="border border-gray-200 p-2">Kotlin</li>
-              <li class="border border-gray-200 p-2">Dart</li>
-            </ul>
+import React from 'react';
+
+const Tools = () => {
+    return (
+        <div className="container mx-auto">
+            <h1>Tools</h1>
+            <hr className="border-t-2 border-blue-500 my-2" />
+            <div id="tools" className="my-2 container grid grid-cols-1 md:grid-cols-3 gap-2 justify-center">
+                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                    <h3>I know the following Languages</h3>
+                    <ul className="list-none">
+                        <li className="border border-gray-200 p-2">Java</li>
+                        <li className="border border-gray-200 p-2">Python</li>
+                        <li className="border border-gray-200 p-2">JavaScript</li>
+                        <li className="border border-gray-200 p-2">GO</li>
+                        <li className="border border-gray-200 p-2">Kotlin</li>
+                        <li className="border border-gray-200 p-2">Dart</li>
+                    </ul>
+                </div>
+                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                    <h3>I know the following Frameworks and Libraries</h3>
+                    <ul className="list-none">
+                        <li className="border border-gray-200 p-2">React</li>
+                        <li className="border border-gray-200 p-2">Laravel</li>
+                        <li className="border border-gray-200 p-2">Django</li>
+                        <li className="border border-gray-200 p-2">Flask</li>
+                        <li className="border border-gray-200 p-2">Flutter</li>
+                    </ul>
+                </div>
+                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                    <h3>I know the following Tools</h3>
+                    <ul className="list-none">
+                        <li className="border border-gray-200 p-2">Git/Github</li>
+                        <li className="border border-gray-200 p-2">MySQL/MariaDB</li>
+                        <li className="border border-gray-200 p-2">Firebase</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full">
-        <h3>I know the following Frameworks and Libraries</h3>
-        <ul class="list-none">
-          <li class="border border-gray-200 p-2">React</li>
-          <li class="border border-gray-200 p-2">Laravel</li>
-          <li class="border border-gray-200 p-2">Django</li>
-          <li class="border border-gray-200 p-2">Flask</li>
-          <li class="border border-gray-200 p-2">Flutter</li>
-        </ul>
-      </div>
-      <div class="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full">
-        <h3>I know the following Tools</h3>
-        <ul class="list-none">
-          <li class="border border-gray-200 p-2">Git/Github</li>
-          <li class="border border-gray-200 p-2">MySQL/MariaDB</li>
-          <li class="border border-gray-200 p-2">Firebase</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-    </>);
-}
+    );
+};
+
 export default Tools;
