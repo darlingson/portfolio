8124adf82cc2f8a4d84b0927f4d7323d0ead0fef
darlingson <dmakuwila@gmail.com>
2023-12-14 21:10:43 +0200

    added type writer library

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,19 @@
+import React from 'react';
+import './Intro.css'; // make sure to import the CSS file
+
 const Intro = (params) => {
-    return(<>
-        <div id="intro" class="container mx-auto flex">
-            <div class="w-1/2">
-                <img class="h-auto max-w-full" src="/12511668-kg.jpg" alt="Darlingson Makuwila"/>
+    return (
+        <div id="intro" className="container mx-auto flex">
+            <div className="w-1/2">
+                <img className="h-auto max-w-full" src="/12511668-kg.jpg" alt="Darlingson Makuwila"/>
             </div>
-            <div class="w-1/2 flex items-center">
-                <p>Hello, My name is Darlingson Makuwila. System Developer and FullStack Web Developer.</p>
+            <div className="w-1/2 flex items-center">
+                <div className="typewriter">
+                    <p>Hello there! I’m Darlingson Makuwila. In the tech world, you’ll find me wearing three hats - one of a System Developer, another of a FullStack Web Developer, and the third of an App Developer. I love diving into systems, untangling complexities, and creating solutions that make a difference.</p>
+                </div>
             </div>
         </div>
-    </>);
-}
+    );
+};
+
 export default Intro;
