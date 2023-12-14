94014bed1bddf12f8086e58c1b8ca90ad0caa0e4
darlingson <dmakuwila@gmail.com>
2023-12-14 21:15:04 +0200

    added type writer effect to intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import './Intro.css'; // make sure to import the CSS file
+import Typewriter from 'typewriter-effect';
 
-const Intro = (params) => {
+const Intro = () => {
     return (
         <div id="intro" className="container mx-auto flex">
             <div className="w-1/2">
                 <img className="h-auto max-w-full" src="/12511668-kg.jpg" alt="Darlingson Makuwila"/>
             </div>
             <div className="w-1/2 flex items-center">
-                <div className="typewriter">
-                    <p>Hello there! I’m Darlingson Makuwila. In the tech world, you’ll find me wearing three hats - one of a System Developer, another of a FullStack Web Developer, and the third of an App Developer. I love diving into systems, untangling complexities, and creating solutions that make a difference.</p>
-                </div>
+                <Typewriter
+                    options={{
+                        strings: ["Hello there! I’m Darlingson Makuwila. In the tech world, you’ll find me wearing three hats - one of a System Developer, another of a FullStack Web Developer, and the third of an App Developer. I love diving into systems, untangling complexities, and creating solutions that make a difference."],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
             </div>
         </div>
     );
