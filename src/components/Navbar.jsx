b81b369b4ce5007d4c2fc9eb2a35a6bdfa895c9c
darlingson <dmakuwila@gmail.com>
2024-01-01 12:41:40 +0200

    modified the code, so that the navbar is initially collapsed on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 const NavBar=()=>{
+  const [isOpen, setIsOpen] = useState(false);
     return(<>
 <nav class="bg-gray-800">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+        <button
+          type="button"
+          class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false"
+          onClick={() => setIsOpen(!isOpen)}
+          >
           <span class="absolute -inset-0.5"></span>
           <span class="sr-only">Open main menu</span>
           <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -27,7 +34,7 @@ const NavBar=()=>{
       </div>
     </div>
   </div>
-  <div class="sm:hidden" id="mobile-menu">
+  <div class={`sm:hidden ${isOpen ? 'block' : 'hidden'}`} id="mobile-menu">
     <div class="space-y-1 px-2 pb-3 pt-2">
       <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Darlingson</a>
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Tools</a>
