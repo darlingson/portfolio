6ebb4981aef1a6420554136b31b5c1150eff411b
darlingson <dmakuwila@gmail.com>
2024-01-01 12:17:10 +0200

    added scroll in animation to the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Projects() {
+    useEffect(() => {
+        AOS.init({
+            delay: 50,
+            mirror: true,
+            easing: 'ease'
+        });
+      }, [])
     return (
         <div className="mx-auto">
             <h1>Projects</h1>
             <hr className="border-t-2 border-blue-500 my-2" />
             <div id="projects" className="container grid grid-cols-1 md:grid-cols-3 gap-2">
-                <Project title="Text to Chichewa Sign Language Translator" desc="This is a Translator which converts text to chichewa sign language" link="http://text-malawisign.000webhostapp.com/view/home.html" />
-                <Project title="Takulandilani App" desc="App built with purpose of the helping users find information about Malawian Tourism. The app has a list of places to visit, accomodation and transportation" />
-                <Project title="Chikulunga Analytics" desc="App built as part of the data science learning journey. Its mainly showing how malawian football is going in the Super league of Malawi." />
-                <Project title="MUBAS Past Papers Repository" desc="A project I worked on for my final year project. The project is a web portal, where students and teachers can access past papers" />
+                <Project data-aos="fade-left" title="Text to Chichewa Sign Language Translator" desc="This is a Translator which converts text to chichewa sign language" link="http://text-malawisign.000webhostapp.com/view/home.html" />
+                <Project data-aos="fade-left" title="Takulandilani App" desc="App built with purpose of the helping users find information about Malawian Tourism. The app has a list of places to visit, accomodation and transportation" />
+                <Project data-aos="fade-left" title="Chikulunga Analytics" desc="App built as part of the data science learning journey. Its mainly showing how malawian football is going in the Super league of Malawi." />
+                <Project data-aos="fade-left" title="MUBAS Past Papers Repository" desc="A project I worked on for my final year project. The project is a web portal, where students and teachers can access past papers" />
             </div>
         </div>
     );
 }
 
 function Project(props) {
+    useEffect(() => {
+        AOS.init();
+      }, [])
     return (
-        <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+        <div data-aos="fade-right" className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
             <ProjectTitle title={props.title} />
             <ProjectDesc desc={props.desc} link={props.link} />
         </div>
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Tools = () => {
+        useEffect(() => {
+        AOS.init();
+      }, [])
     return (
         <div className="container mx-auto">
             <h1>Tools</h1>
             <hr className="border-t-2 border-blue-500 my-2" />
             <div id="tools" className="my-2 container grid grid-cols-1 md:grid-cols-3 gap-2 justify-center">
-                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                <div data-aos="fade-right" className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
                     <h3>I know the following Languages</h3>
                     <ul className="list-none">
                         <li className="border border-gray-200 p-2">Java</li>
@@ -17,7 +22,7 @@ const Tools = () => {
                         <li className="border border-gray-200 p-2">Dart</li>
                     </ul>
                 </div>
-                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                <div data-aos="fade-right" className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
                     <h3>I know the following Frameworks and Libraries</h3>
                     <ul className="list-none">
                         <li className="border border-gray-200 p-2">React</li>
@@ -27,7 +32,7 @@ const Tools = () => {
                         <li className="border border-gray-200 p-2">Flutter</li>
                     </ul>
                 </div>
-                <div className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
+                <div data-aos="fade-right" className="bg-sky-300 hover:bg-sky-700 border border-gray-200 w-full p-2">
                     <h3>I know the following Tools</h3>
                     <ul className="list-none">
                         <li className="border border-gray-200 p-2">Git/Github</li>
